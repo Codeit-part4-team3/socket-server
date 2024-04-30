06c6612dc055afd4847ed64643287f9053740760
giho <giho7427@daum.net>
2024-04-30 14:28:02 +0900

    feat: chat channel 채팅 CRUD중 create 기능 구현 및 입장시 기존 채널의 채팅 메시지 가져오기 구현
    (cherry picked from commit 60aef40daa49e602d7aebc75be36452af5c5f345)

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const crypto = require("crypto");
 
 // .DocumentClient를 사용하면 DynamoDB의 데이터를 쉽게 다룰 수 있다. 자동 직렬화 느낌
 const dynamoDB = new AWS.DynamoDB.DocumentClient({
-    // 다이나모DB의 리전을 설정한다
+    // 다이나모DB의 리전을 설정한다(다이나모 DB가 존재하는 지역)
     region: "us-east-2",
 });
 const app = express();
@@ -16,50 +16,13 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         // origin은 정확한 클라이언트 주소여야한다.
-        origin: "https://pqsoft.net",
+        origin: ["https://pqsoft.net", "http://localhost:5173"],
         methods: ["GET", "POST", "PUT", "DELETE"],
     },
 });
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
-});
-
-// dynamoDB의 get, query, scan메서드를 이용해서 데이터를 가져올 수 있는데
-// get은 단일 항목을 가져오고
-// query는 쿼리 조건에 맞는 항목을 가져오고
-// scan은 테이블 전체를 가져온다.
-// 따라서 특정 조건을 줘서 데이터를 가져오고 싶을 때는 query를 사용해야한다
-app.get("/admin/messages", (req, res) => {
-    const params = {
-        TableName: "chat-message-table",
-        // #channel이라는 표현식이 :channelId와 같은지 확인한다.
-        KeyConditionExpression: "#channel = :channelId",
-        // channelId가 예약어이기 때문에 ExpressionAttributeNames를 사용해서 예약어를 대체한다.
-        ExpressionAttributeNames: {
-            "#channel": "channelId",
-        },
-        // admin 채널의 메시지를 가져오기 위해 channelId를 admin으로 설정한다.
-        ExpressionAttributeValues: {
-            ":channelId": "admin_chat_channel",
-        },
-    };
-    // query메서드를 사용해서 데이터를 가져온다.
-    dynamoDB.query(params, (err, data) => {
-        if (err) {
-            console.error("Error fetching data from DynamoDB:", err);
-            res.status(500).send("Error fetching data from DynamoDB");
-        } else {
-            console.log("Data fetched successfully from DynamoDB:", data);
-            // Set CORS headers
-            res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
-            res.setHeader(
-                "Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE"
-            ); // Allow the specified HTTP methods
-            res.status(200).send(data.Items); // Send the fetched messages as response
-        }
-    });
 });
 
 const pc_config = {
@@ -224,100 +187,88 @@ io.on("connect", (socket) => {
     );
 
     // 채팅 채널 관련 코드
-    socket.on("join_chat_channel", (roomName) => {
+    socket.on("join_chat_channel", async (roomName) => {
+        console.log("join_chat_channel : ", roomName);
         // DB에서 channel 데이터 가져오기
         // DB에서 채팅 메시지 데이터 가져오기
         // 채팅 메시지 소켓으로 보내기
         socket.join(roomName);
+        socket.emit("join_chat_channel_user_join", socket.id);
+
+        // DB에서 해당 채널의 메시지를 가져와서 참가한 소켓에게 보내기
+        const queryParams = {
+            TableName: "chat-channel-table",
+            KeyConditionExpression: "channelId = :channelId",
+            ExpressionAttributeValues: {
+                ":channelId": roomName, // roomName이 ChannelId가 된다.
+            },
+        };
+
+        try {
+            const data = await dynamoDB.query(queryParams).promise();
+            console.log("Messages fetched successfully from DynamoDB:", data);
+            // 방금 참가한 소켓에게만 메시지를 보낸다.
+            const initialMessages = data.Items;
+            io.to(socket.id).emit("initial_chat_messages", initialMessages);
+        } catch (error) {
+            console.error("Error fetching messages from DynamoDB:", error);
+        }
     });
 
+    // interface MessageItem {
+    //     channelId: string;
+    //     messageId: string;
+    //     message: {
+    //       userId: string;
+    //       message: string;
+    //       createdAt: number;
+    //       updatedAt: number;
+    //       status: string; : 'stable' | 'editing
+    //     };
+    //   }
     socket.on("send_message", async (message, roomName) => {
         // 새로운 메시지를 DynamoDB에 업데이트
-        const newMessage = {
-            messageId: generateMessageId(),
-            userId: "1",
-            message: message,
-            createdAt: Date.now(),
-            updatedAt: Date.now(),
-            status: "normal",
+        const messageId = generateMessageId();
+        const newMessageItem = {
+            channelId: roomName, // Use roomName as the channelId
+            messageId: messageId,
+            message: {
+                createdAt: Date.now(),
+                message: message,
+                status: "stable",
+                updatedAt: Date.now(),
+                userId: "1", // Assuming a static userId for now
+            },
         };
-        console.log(newMessage);
+
+        console.log("newMessage:", newMessageItem);
+
         // roomName에 속한 모든 소켓에게 메시지를 보낸다.(보낸 사람 포함)
-        io.in(roomName).emit("receive_message", newMessage);
+        io.in(roomName).emit("receive_message", newMessageItem);
 
-        const updateParams = {
-            // 테이블 이름
-            TableName: "chat-message-table",
-            // channelID을 이용해서 해당 채널의 메시지 리스트에 새로운 메시지를 추가한다.
-            Key: {
-                channelId: roomName,
-            },
-            // updateExpression은 SET으로 업데이트할 데이터를 정의한다.
-            UpdateExpression:
-                // list_append는 DynamoDB에서 제공하는 함수로 리스트에 새로운 요소를 추가한다.
-                // if_not_exists는 DynamoDB에서 제공하는 함수로 해당 속성이 존재하지 않으면
-                // 두 번째 인자로 전달된 값을 사용한다.
-                // :newMessage는 새로운 메시지를 의미한다.
-                "SET messages = list_append(if_not_exists(messages, :emptyList), :newMessage)",
-            // ExpressionAttributeValues는 UpdateExpression에서 사용한 변수들을 정의한다.
-            // :newMessage는 새로운 메시지를 의미한다.
-            // :emptyList는 messages 속성이 존재하지 않을 때 사용할 빈 리스트를 의미한다.
-            ExpressionAttributeValues: {
-                ":newMessage": [newMessage],
-                ":emptyList": [],
-            },
-            // ReturnValues는 업데이트 후 반환할 데이터를 정의한다.
-            ReturnValues: "UPDATED_NEW",
+        const putParams = {
+            TableName: "chat-channel-table", // 테이블 이름
+            Item: newMessageItem, // 새로운 메시지
         };
 
-        // DynamoDB에 데이터를 업데이트한다.
-        dynamoDB.update(updateParams, (err, data) => {
-            if (err) {
-                console.error("Error updating data in DynamoDB:", err);
-            } else {
-                console.log("Data updated successfully:", data);
-            }
-        });
+        console.log("Adding a new message to DynamoDB:", putParams);
+
+        try {
+            const data = await dynamoDB.put(putParams).promise();
+            console.log("Message added successfully to DynamoDB:", data);
+        } catch (error) {
+            console.error("Error adding message to DynamoDB:", error);
+        }
     });
 
     // update Message
     socket.on("update_message", (messageId, roomName) => {
-        // update Message
+        // Logic
     });
 
     // delete Message
     socket.on("delete_message", (messageId, roomName) => {
-        const deleteParams = {
-            TableName: "chat-message-table",
-            Key: {
-                channelId: roomName,
-            },
-            // 업데이트 작업을 정의한다.
-            // messages 목록 속성에서 ?인덱스 요소를 제거한다
-            UpdateExpression: "REMOVE messages[?]",
-            // 삭제 작업이 수행되기 위해 필요한 조건을 정의한다.
-            // 여기서는 messages 속성이 존재하고 messages 목록에 :messageId가 포함되어 있는지 확인한다.
-            ConditionExpression:
-                "attribute_exists(messages) AND list_contains(messages, :messageId)",
-            // Expression들의 :messageId을 정의한다
-            ExpressionAttributeValues: {
-                ":messageId": messageId,
-            },
-        };
-
-        // delete메서드를 안사용한 이유는 delete는 전체 항목(또는 행)를 삭제하려고 할 때 사용하고
-        // update메서드는 항목 내부의 속성을 업데이트하거나 제거할 때 사용할 수 있기 떄문
-        // 우리는 channelId 내보의 messages 속성에서 특정 메시지를 제거하려고 하기 때문에 update메서드를 사용한다.
-        dynamoDB.update(deleteParams, (err, data) => {
-            if (err) {
-                console.error("Error deleting message from DynamoDB:", err);
-            } else {
-                console.log(
-                    "Message deleted successfully from DynamoDB:",
-                    data
-                );
-            }
-        });
+        // Logic
     });
 });
 
